cc1dc5c481e67454fc1d8d2402f94060510958f0
Lev Staver <levstaver@yandex.ru>
2024-12-07 22:28:15 +0500

    feat: complete 5 task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -156,8 +156,53 @@ function convertToRomanNumerals(num) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  let str = '';
+  for (let i = 0; i < numberStr.length; i += 1) {
+    switch (numberStr[i]) {
+      case '-':
+        str += 'minus ';
+        break;
+      case '0':
+        str += i === numberStr.length - 1 ? 'zero' : 'zero ';
+        break;
+      case '1':
+        str += i === numberStr.length - 1 ? 'one' : 'one ';
+        break;
+      case '2':
+        str += i === numberStr.length - 1 ? 'two' : 'two ';
+        break;
+      case '3':
+        str += i === numberStr.length - 1 ? 'three' : 'three ';
+        break;
+      case '4':
+        str += i === numberStr.length - 1 ? 'four' : 'four ';
+        break;
+      case '5':
+        str += i === numberStr.length - 1 ? 'five' : 'five ';
+        break;
+      case '6':
+        str += i === numberStr.length - 1 ? 'six' : 'six ';
+        break;
+      case '7':
+        str += i === numberStr.length - 1 ? 'seven' : 'seven ';
+        break;
+      case '8':
+        str += i === numberStr.length - 1 ? 'eight' : 'eight ';
+        break;
+      case '9':
+        str += i === numberStr.length - 1 ? 'nine' : 'nine ';
+        break;
+      case '.':
+      case ',':
+        str += 'point ';
+        break;
+      default:
+        str = '';
+    }
+  }
+
+  return str;
 }
 
 /**
